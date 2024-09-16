83eac9a1f24f74e3a1f215a093e7b49c919f2fc8
himanish24 <himanish1724@gmail.com>
2024-09-16 17:24:22 +0530

    first commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'The World Map 2.0' });
 });
+router. get('/location',function(req,res,next){
+  res.render('index',{title:'Location Info'});
+});
+router.get('/location/review',function(req,res,next){
+  res.render('index',{title:'Review'});
+});
+router.get('/about',function(req,res,next){
+  res.render('index',{title:'About'});
+});
 
 module.exports = router;
